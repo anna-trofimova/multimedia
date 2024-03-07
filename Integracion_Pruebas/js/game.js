9b4054f11fd650998b0f5b4390c59e8a67bfbb08
Kr4ll <albertoopx@gmail.com>
2024-03-07 18:18:09 +0100

    css adjust

--- Integracion_Pruebas/js/game.js
+++ Integracion_Pruebas/js/game.js
@@ -228,7 +228,7 @@ var game = {
 			game.panTo(heroX);
 
 			//Y esperar hasta que deja de moverse o está fuera de los límites
-			if(!game.currentHero.IsAwake() || heroX<0 || heroX >game.currentLevel.foregroundImage.width ){
+			if(!game.currentHero.IsAwake() || heroX<0 || heroX >game.currentLevel.foregroundImage.width || ){
 				// Luego borra el viejo héroe
 				box2d.world.DestroyBody(game.currentHero);
 				game.currentHero = undefined;
@@ -421,7 +421,6 @@ var settings = {
 
 	changeSound:function(selected){
 		game.backgroundMusic = loader.loadSound(selected)
-		game.startBackgroundMusic();
 	}
 
 	
